65c90d69a33a27a92d0fa4dbfe94d77265a0a0d6
PabloSarsotoMa <p.sarsoto.m@gmail.com>
2023-01-09 14:46:49 +0100

    gg

--- src/controller/viajes.controller.js
+++ src/controller/viajes.controller.js
@@ -40,7 +40,7 @@ function getViaje(request,response){
 }
 
 function getTarjetaViaje(request, response) {
-    let sql = `SELECT * FROM viaje JOIN usuarios ON (viaje.id_usuarios=usuarios.id_usuario) JOIN coche ON (usuarios.id_usuario = coche.id_conductor) WHERE viaje.id_viaje=${request.query.id_viaje} AND coche.nombreCoche = viaje.id_coche `;
+    let sql = `SELECT * FROM viaje JOIN usuarios ON (viaje.id_usuarios=usuarios.id_usuario) JOIN coche ON (viaje.id_coche = coche.nombreCoche)  WHERE viaje.id_viaje=${request.query.id_viaje}`;
 
     connection.query(sql,function(err, result){
         if(err){
