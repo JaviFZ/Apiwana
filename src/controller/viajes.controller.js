fa1e6da44b241bc7467797b9ea9eab4be383f87e
JaviFZ <jferzap@gmail.com>
2023-01-10 23:50:45 +0100

    cambio en getpasajeros

--- src/controller/viajes.controller.js
+++ src/controller/viajes.controller.js
@@ -95,7 +95,7 @@ function getMisViajesPublicados(request,response){
     })
 }
 function getPasajeros(request, response) {
-    let sql = "SELECT id_usuario1, foto, nombre FROM railway.chats join railway.usuarios ON (railway.chats.id_usuario1 = railway.usuarios.id_usuario) JOIN railway.viaje ON (railway.chats.id_usuario2 = railway.viaje.id_usuarios) WHERE viaje.id_viaje= " + request.query.id_viaje;
+    let sql = "SELECT id_usuario1, foto, nombre FROM railway.chats join railway.usuarios ON (railway.chats.id_usuario1 = railway.usuarios.id_usuario) JOIN railway.viaje ON (railway.chats.id_usuario2 = railway.viaje.id_usuarios) WHERE viaje.id_viaje= " + request.body.id_viaje;
       console.log(sql);  
       connection.query(sql, function (err, result) {
           if (err) 
