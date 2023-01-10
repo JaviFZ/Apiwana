63bd1299b8e2d96f625f99b46ab3ea2fa2344b19
JaviFZ <jferzap@gmail.com>
2023-01-10 15:52:43 +0100

    actualizaciones para viajes publicados

--- src/controller/viajes.controller.js
+++ src/controller/viajes.controller.js
@@ -75,5 +75,36 @@ function getMisViajes(request,response){
     })
 }
 
+function getMisViajesPublicados(request,response){
+    console.log(request.query);
+    let sql = `SELECT viaje.*, usuarios.foto, usuarios.nombre FROM railway.viaje
+    JOIN usuarios ON (viaje.id_usuarios=usuarios.id_usuario) JOIN chats ON 
+    (viaje.id_viaje=chats.id_viaje) WHERE chats.id_usuario2=${parseInt(request.query.id_usuario)};`;
+    connection.query(sql,function(err, result){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(result);
+            if(result){
+                response.send(result);
+            }else{
+                response.send("-1");
+            }
+        }
+    })
+}
+function postPasajeros(request, response) {
+    let sql = "SELECT id_usuario1, foto FROM railway.chats join railway.usuarios ON (railway.chats.id_usuario1 = railway.usuarios.id_usuario) JOIN railway.viaje ON (railway.chats.id_usuario2 = railway.viaje.id_usuarios) WHERE viaje.id_viaje= " + request.body.id_viaje;
+      console.log(sql);  
+      connection.query(sql, function (err, result) {
+          if (err) 
+              console.log(err);
+          else {
+              console.log(result)
+              response.send(result);
+          }
+      })
+  }
 
-module.exports = {postViaje, getViaje, getTarjetaViaje, getMisViajes}
+
+module.exports = {postViaje, getViaje, getTarjetaViaje, getMisViajes, getMisViajesPublicados, postPasajeros}
