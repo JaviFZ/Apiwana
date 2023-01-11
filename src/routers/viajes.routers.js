aeb98484e6591813de5a774a7f218a6d5ed86650
Fanel Adrian <faneladrianpop@gmail.com>
2023-01-11 22:19:39 +0100

    borrar viaje mal

--- src/routers/viajes.routers.js
+++ src/routers/viajes.routers.js
@@ -7,8 +7,9 @@ router.get("/viajes",viajesCtrl.getViaje);
 router.get("/misViajes",viajesCtrl.getMisViajes);
 router.get("/viaje",viajesCtrl.getTarjetaViaje);
 router.get("/viajePublicado",viajesCtrl.getMisViajesPublicados);
+router.get("/viajesPublicados",viajesCtrl.getPasajeros);
 router.delete("/misViajes", viajesCtrl.deleteViajePasajero);
 router.delete("/viajesPublicados", viajesCtrl.deleteViajePublicado);
-router.get("/viajesPublicados",viajesCtrl.getPasajeros);
+
 
 module.exports = router;
