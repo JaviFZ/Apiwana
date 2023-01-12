fbc5aa9b82eefbd6b828a6b4f9816903cbd5525d
JaviFZ <jferzap@gmail.com>
2023-01-12 15:19:19 +0100

    cambios api

--- src/dataBase.js
+++ src/dataBase.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2")
 
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || "iwanacar.cv6m2ktduioy.us-east-1.rds.amazonaws.com",
+    host: process.env.DB_HOST ,
     user: process.env.DB_USER || "admin",
     password: process.env.DB_PASSWORD || "iwanacar4.",
     database: process.env.DB_NAME || "railway",
