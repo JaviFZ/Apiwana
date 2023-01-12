6debe4c5f848ec742e5eb7f0da888b6dba3205e4
JaviFZ <jferzap@gmail.com>
2023-01-12 13:20:03 +0100

    aws

--- src/dataBase.js
+++ src/dataBase.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST || "iwanacar.cv6m2ktduioy.us-east-1.rds.amazonaws.com",
     user: process.env.DB_USER || "admin",
     password: process.env.DB_PASSWORD || "iwanacar4.",
-    database: process.env.DB_NAME || "aws",
+    database: process.env.DB_NAME || "railway",
     port: process.env.DB_PORT || 3306
 })
 
